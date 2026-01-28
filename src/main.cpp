791311d2cea8140248cdf45fe5de84f50e9e5ee9
john-gregtech <williamtberry05@gmail.com>
2026-01-28 11:20:05 +1000

    generating a key in aes256

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <array>
 #include "cryptowrapper/aes256.h"
 
 int main() {
     std::cout << "test" << std::endl;
 
     prototype_functions::openssl_sanity_check();
+    std::array<unsigned char, 32> my_key{};
+
+    my_key = prototype_functions::generate_key();
+    for (unsigned char i : my_key) {
+        std::cout << i;
+    }
+    std::cout << '\n';
 
     return 0;
 }
