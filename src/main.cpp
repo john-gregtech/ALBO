46701083abf8a545c77c39c00ff399c34e3c94a6
john-gregtech <williamtberry05@gmail.com>
2026-01-30 14:17:38 +1000

    sha256 test

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,26 @@
 #include <array>
 #include <sstream>
 #include <stdexcept>
+#include <bits/stdc++.h>
 #include <iomanip>
 
 #include "cryptowrapper/sha256.h"
 
-template <unsigned int x>
-std::string hexarraytostring(const std::array<unsigned char, x>& data) {
+template <unsigned int arrayLength> 
+std::string hexarraytostring(const std::array<unsigned char, arrayLength>& data) {
     std::stringstream ss;
-    ss << std::hex;
+    ss << std::hex << std::setfill('0');
 
-    for (int i = 0; i < x; ++i) {
-        ss << std::setw(2) << std::setfill('0') << data.at(i);
+    for (unsigned int i = 0; i < arrayLength; ++i) {
+        // Cast to int to print numeric value instead of character
+        ss << std::setw(2) << static_cast<int>(data[i]);
     }
+
     return ss.str();
 }
-
 int main() {
     std::string message = "This is a handshake";
+    std::getline(std::cin, message);
     std::string stringhash{};
     std::array<unsigned char, 32> hash{};
 
