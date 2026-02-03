4edd051b1c6c64698696084a524a3005863753f0
john-gregtech <williamtberry05@gmail.com>
2026-02-03 13:25:30 +1000

    testing to see if windows api was properly working and it is

--- src/network/win/client.cpp
+++ src/network/win/client.cpp
@@ -1,7 +1,25 @@
+// Minimal client example
+#include <winsock2.h>
 #include <iostream>
+// #pragma comment(lib, "ws2_32.lib")
 
 int main() {
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
 
-    std::cout << "client.exe\n";
-    return 61;
+    SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(5555);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    connect(s, (sockaddr*)&addr, sizeof(addr));
+
+    char buffer[512] = {};
+    int received = recv(s, buffer, sizeof(buffer), 0);
+    if (received > 0)
+        std::cout << "Received: " << std::string(buffer, received) << "\n";
+
+    closesocket(s);
+    WSACleanup();
 }
--- src/network/win/server.cpp
+++ src/network/win/server.cpp
@@ -1,7 +1,58 @@
 #include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+// #pragma comment(lib, "ws2_32.lib")
 
 int main() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed\n";
+        return 1;
+    }
 
-    std::cout << "server.exe\n";
-    return 67;
+    SOCKET ListenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (ListenSocket == INVALID_SOCKET) {
+        std::cerr << "Error creating socket\n";
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(5555); // Change port if needed
+
+    if (bind(ListenSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "Bind failed\n";
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
+        std::cerr << "Listen failed\n";
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Server listening on port 5555...\n";
+
+    while (true) {
+        SOCKET ClientSocket = accept(ListenSocket, nullptr, nullptr);
+        if (ClientSocket == INVALID_SOCKET) {
+            std::cerr << "Accept failed\n";
+            break;
+        }
+
+        const char* reply = "Hello";
+        if (send(ClientSocket, reply, (int)strlen(reply), 0) == 5)
+            std::cout << "connection\n";
+        closesocket(ClientSocket);
+    }
+
+    closesocket(ListenSocket);
+    WSACleanup();
+    return 0;
 }
