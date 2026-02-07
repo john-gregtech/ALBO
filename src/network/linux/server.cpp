991e6ebd2e196740764a14b140ce2809c76aa47e
John-gregtech <212177740+john-gregtech@users.noreply.github.com>
2026-02-07 13:51:01 +1000

    need to add mutex to the thread stuff

--- src/network/linux/server.cpp
+++ src/network/linux/server.cpp
@@ -6,13 +6,9 @@
 #include <math.h>
 #include <thread>
 #include <mutex>
+#include <memory>
 
-//basically it needs to be threaded so there will be a producer and consumer thread
-//consumer will be the main thread
-//producer will be the file reader thread
-//seekg to got eg seekg(-1, std::ios::cur) is move back one byte
-
-class FileStream {
+class FileReadStream {
 private:
     static constexpr size_t chunkSize = 16 * 1024;
 
@@ -21,9 +17,13 @@ private:
 
     std::vector<int> readBuffer;       //buffer that reads the file
     std::vector<int> transferBuffer;   //this one takes the data from readBuffer
+
+    std::thread readWorker;
     
-    bool transferLock = false;
-    bool readDone = false;
+    bool transferLock       = true; //we do first round lock cause we didnt do a do while
+    bool readReady          = false;
+    bool readDone           = false; //this is for later if we need to check if the thread is ready to transfer
+    bool fileFullyRead      = false;
     size_t currentChunk     = 0; //done
     size_t chunkCount       = 0; //done
     size_t lastChunkSize    = 0; //done
@@ -38,73 +38,93 @@ private:
         chunkCount = static_cast<size_t>(std::ceil(static_cast<double>(totalFileSize)/static_cast<double>(chunkSize)));
         lastChunkSize = totalFileSize-(chunkCount-1)*chunkSize;
     }
-
-    //This function will be the function to be called as a thread
+    //the function that is actually threaded
     void scanChunk() {
-        //goto start
+        std::cout << "\nH" << chunkCount << "\n";
         file.seekg(0, std::ios::beg);
-        //first which chunk are we on
-        //if chunkcount is 1 then lastchunksize is the first chunk so then we will use that
-        //calculate chunk data was pre called instantly after the file was opened
         for (currentChunk = 1; currentChunk <= chunkCount; ++currentChunk) {
-            //we have the parts that read now we need to actually put it into the buffer
-            //first we clear the buffer
+
             readDone = false;
             readBuffer.clear();
 
-            //std::cout << currentChunk << std::endl;
-            if (currentChunk == chunkCount) {
+            if (currentChunk != chunkCount) {
                 readBuffer.resize(lastChunkSize);
                 for (size_t byteCursor = 0; byteCursor < lastChunkSize; ++byteCursor) {
                     readBuffer.at(byteCursor) = file.get();
-                    std::cout << readBuffer.at(byteCursor);
                 }
             } else {
                 readBuffer.resize(chunkSize);
                 for (size_t byteCursor = 0; byteCursor < chunkSize; ++byteCursor) {
                     readBuffer.at(byteCursor) = file.get();
-                    std::cout << readBuffer.at(byteCursor);
                 }
             }
             readDone = true;
-            //now we have a full buffer
-            //this is the point where we wait
-            std::cout << "At lock\n";
             while (transferLock) {
-                //wait code
                 std::this_thread::sleep_for(std::chrono::microseconds(2));
             }
             transferBuffer.assign(readBuffer.begin(), readBuffer.end());
             
-            transferLock = true; //imagine it gets checked now
-            std::cout << "Past lock\n";
+            transferLock = true;
         }
-        
+        fileFullyRead = true;
     }
 public:
-    explicit FileStream(const std::string& _fn) : file(_fn, std::ios::binary | std::ios::ate) {
+    explicit FileReadStream(const std::string& _fn) : file(_fn, std::ios::binary | std::ios::ate) {
         fn = _fn;
         if (!file.is_open()) {
-            std::cerr << "Error, File failed to open somehow";
+            std::cerr << "Error, File failed to open";
         }
-        std::cerr << "working and good\n";
         calculateChunkData();
-        scanChunk();
     }
-    ~FileStream() {
+    ~FileReadStream() {
         file.close();
         if (!file.is_open()) {
             std::cerr << "File has been closed\n";
         } else {
             std::cerr << "Not closed oh o\n";
         }
-        //also close threads here incase they are still runnning
+        join_thread();
+    }
+    int getTransferBuffer(std::vector<int>* _buffer) {
+        while (!transferLock) {
+
+        }
+        if (!transferLock) {
+            return 0;
+        } else {
+            *_buffer = transferBuffer;
+            transferLock = false;
+        }
+        return 1;
+    }
+    void start_thread() {
+        readWorker = std::thread(&FileReadStream::scanChunk, this);
+    }
+    void join_thread() {
+        if (readWorker.joinable()) readWorker.join();
+    }
+    bool isReadyToTransfer() {
+        return readDone;
+    }
+    bool isDone() { //lol at the name mismatch
+        return fileFullyRead;
     }
     
 };
 
-
 int main() {
-    FileStream fs("yummy.txt");
+    std::unique_ptr<FileReadStream> fs = std::make_unique<FileReadStream>("large.rar");
+    fs->start_thread();
+    std::vector<int> chunk{};
+    // std::cout << (bool)fs->isDone() << " This is the the isDone()\n";
+    int i{};
+    while (!fs->isDone()) {
+        std::cout << fs->getTransferBuffer(&chunk) << "if fail -> ";
+        std::cout << "AAA" << ++i << "\n";
+    }
+
     
+
+
+
 }
