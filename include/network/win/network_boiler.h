f5453b92a697961db73d25d0136ee3e5ebae9645
john-gregtech <johngregtech@users.noreply.github.com>
2026-03-01 13:07:11 +1000

    a

--- include/network/win/network_boiler.h
+++ include/network/win/network_boiler.h
@@ -3,45 +3,48 @@
 #include <ws2tcpip.h> // for inet_pton
 #include <iostream>
 #include <string>
+#include <cstdint>
 #include <vector>
 
-struct Ip {
-    uint32_t ip_host = 0xC0A80105;         // host order
-    uint32_t ip_network = htonl(ip_host);  // network order
-};
+namespace prototype::network {
+    struct Ip {
+        uint32_t ip_host{};// example of how it would look 0xC0A80105;
+        uint32_t ip_network() const;
+    };
+    
 
-//Socket Information such as holding information and status and passing data through
-class Socket {
-public:
+    //Socket Information such as holding information and status and passing data through
+    class Socket {
+    public:
 
-private:
-    SOCKET handle;
-    bool open;
-    std::string peerID;
-    int peerPort;
-};
+    private:
+        SOCKET handle;
+        bool open;
+        Ip peerIp; //optional
+        int peerPort; //optional
+    };
 
-//Manages Sockets based on what we want them to do closes and opens sockets and stores an index of them
-std::vector<Socket> socket_list{};
+    //Manages Sockets based on what we want them to do closes and opens sockets and stores an index of them
+    std::vector<Socket> socket_list{};
 
-//(we need a ) + x
-//we need a list of sockets
+    //(we need a ) + x
+    //we need a list of sockets
 
 
-//This class will contain basic server information such as pointers to targeted ports/sockets and ip
-//Later down the line this can be expanded
-class Server {
-public:
+    //This class will contain basic server information such as pointers to targeted ports/sockets and ip
+    //Later down the line this can be expanded
+    class Server {
+    public:
 
-private:
+    private:
 
-};
+    };
 
-//Similar situation to the server class but Client focused
-class Client {
-public:
+    //Similar situation to the server class but Client focused
+    class Client {
+    public:
 
-private:
+    private:
 
-};
-
+    };
+}
--- src/network/win/network_boiler.cpp
+++ src/network/win/network_boiler.cpp
@@ -1,1 +1,7 @@
-#include "network/windows"
+#include "network/win/network_boiler.h"
+
+namespace prototype::network {
+
+    uint32_t Ip::ip_network() const { return htonl(ip_host); }
+
+}
